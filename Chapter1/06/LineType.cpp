bd42e68d517b363f40ee07e214ba186fc8aedf17
dissolvdgrl <c@chilldsgn.com>
2025-04-02 21:08:05 +0200

    Start question 7 and update project

---
+++ Chapter1/06/LineType.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Christie on 2023/05/04.
+//
+
+#include "LineType.h"
---
+++ Chapter1/06/LineType.h
@@ -0,0 +1,14 @@
+//
+// Created by Christie on 2023/05/04.
+//
+
+#ifndef INC_06_LINETYPE_H
+#define INC_06_LINETYPE_H
+
+
+class LineType {
+
+};
+
+
+#endif //INC_06_LINETYPE_H
